58be270e9e85f6e6ffcf2cfa952f6468fce8aa6e
crystal <crystal@hhzhhz.cn>
2018-12-17 20:38:40 +0800

    tijio

--- runoob_practice_02.py
+++ runoob_practice_02.py
@@ -10,3 +10,15 @@
 # 从键盘输入当月利润I，求应发放奖金总数？
 
 i = int(raw_input('净利润:'))
+arr = [1000000,600000,400000,200000,100000,0]
+rat = [0.01,0.015,0.03,0.05,0.075,0.1]
+r = 0
+for idx in range(0,6):
+    if i>arr[idx]:
+        print idx
+        r+=(i-arr[idx])*rat[idx]
+        print "== r == ", r
+        i=arr[idx]
+        print "== i == ", i
+
+print r
